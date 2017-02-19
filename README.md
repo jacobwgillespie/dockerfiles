e938378619ed4bacb002084a29db71bb50310556
Jacob Gillespie <jacobwgillespie@gmail.com>
2017-02-19 15:12:13 -0600

    Allow customization of VNC port

--- README.md
+++ README.md
@@ -22,8 +22,9 @@ Variable | Default | Description
 `TZ` | none | Set this to control the timezone in the container
 `PUID` | none | Set the UID of the container user
 `PGID` | none | Set the GID of the container user
-`VNC_RESOLUTION` | `1920x1080` | The display resolution for qBittorrent
 `VNC_PASSWD` | none | If set, the VNC server will require a password
+`VNC_PORT` | 5900 | Change the VNC server port
+`VNC_RESOLUTION` | `1920x1080` | The display resolution for qBittorrent
 
 ### Ports
 
--- files/tmp/qbittorrent-install.sh
+++ files/tmp/qbittorrent-install.sh
@@ -8,6 +8,7 @@ apt-get install -qy --force-yes qbittorrent
 # Add service to init
 cat <<'EOT' > /etc/init.d/qbittorrent
 #!/bin/bash
+source /opt/default-values.sh
 case "$1" in
   start)
     /usr/bin/sv start crashplan
@@ -22,7 +23,7 @@ case "$1" in
     /usr/bin/sv restart openbox
     ;;
   status)
-    eval 'exec 6<>/dev/tcp/127.0.0.1/5900 && echo "running" || echo "stopped"' 2>/dev/null
+    eval 'exec 6<>/dev/tcp/127.0.0.1/${VNC_PORT} && echo "running" || echo "stopped"' 2>/dev/null
     exec 6>&- # close output connection
     exec 6<&- # close input connection
     ;;
